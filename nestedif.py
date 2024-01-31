d4e2b029502d8fe54f00e0e9cd465ca440bb6659
bala2051005E <ethistr007@gmail.com>
2024-01-31 20:38:24 +0530

    new program

---
+++ nestedif.py
@@ -0,0 +1,13 @@
+a=10
+b=20
+c=30
+if a>b and a>c:
+    print(f"a value is {a} is greater than all")
+    if b>c:
+        print(f"c value is {c} is smaller than all")
+    else:
+        print(f"b value is {b} is smaller than all")
+else b>c:
+    print(f"b value is {b} greater than all")
+    
+    
